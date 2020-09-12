a69ee880d3fa61fe61dd8f173a25a52461c7a4a8
Sam Stevens <stevens.994@osu.edu>
2020-09-12 16:30:13 -0400

    added twisting dials

--- watsong/jukebox.py
+++ watsong/jukebox.py
@@ -2,30 +2,39 @@
 This is the main controller (called blueprints in Flask) for the application.
 """
 
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, jsonify, session
 
 from . import watson, spotify
 
 bp = Blueprint("jukebox", __name__, url_prefix="/jukebox")
 
+DIALS = ["energy", "lyrics", "dance", "melody"]
 
-@bp.route("/", methods=["GET"])
+
+@bp.route("/", methods=["GET", "POST"])
 def jukebox():
-    return render_template("jukebox.html")
+    """
+    Renders the empty jukebox page.
+    """
+    songs = []
 
+    if request.method == "POST":
+        query = request.form["query"]
 
-@bp.route("/_nat_lang_query", methods=["GET"])
-def nat_lang_query():
-    query = request.args.get("query", "", type=str)
+        print(query)
 
-    albums, err = watson.get_albums(query)
+        if query:
+            albums, err = watson.get_albums(query)
 
-    if err is not None:
-        return jsonify({"error": str(err)})
+            if err is not None:
+                return jsonify({"error": str(err)})
 
-    songs, err = spotify.get_songs(albums)
+            songs, err = spotify.get_songs(albums)
 
-    if err is not None:
-        return jsonify({"error": str(err)})
+            if err is not None:
+                return jsonify({"error": str(err)})
 
-    return jsonify(songs)
+            session.clear()
+            session["songs"] = songs
+
+    return render_template("jukebox.html", songs=songs, dials=DIALS)
