53cf11e83fd11be5f5065c3a23f1e9705de10269
Sam Stevens <stevens.994@osu.edu>
2020-09-15 19:55:49 -0400

    updated envs

--- envs/dev-unix.sh
+++ envs/dev-unix.sh
@@ -2,5 +2,5 @@
 export FLASK_APP=watsong
 export FLASK_ENV=development
 export SECRET_KEY=dev
-export SPOTIPY_CLIENT_ID="cabbb7f1ed864a9d8cd4db6ec60ae7da"
-export SPOTIPY_CLIENT_SECRET="87edc5fa9ab6409784f1c11237e77515"
+export SPOTIPY_CLIENT_ID="5d141ce6f09c4fbfa12d16ce9e5d40c1"
+export SPOTIPY_CLIENT_SECRET="52fed0d6564a4e6f8e596b78bd1abf62"
--- envs/dev-windows.bat
+++ envs/dev-windows.bat
@@ -1,5 +1,3 @@
-REM I'm not sure how to best export variables in windows. Refer to https://flask.palletsprojects.com/en/1.1.x/tutorial/factory/#run-the-application
-
 set FLASK_APP=watsong
 set FLASK_ENV=development
 set SECRET_KEY=dev
