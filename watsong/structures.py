7188133b780c406a593745f008ea465c980e0681
Alex Li <7Alex7Li@gmail.com>
2020-09-18 14:38:17 -0400

    Update structures.py

--- watsong/structures.py
+++ watsong/structures.py
@@ -6,12 +6,6 @@ It also includes some generic types that can be used for error handling, such as
 from typing import NamedTuple, List, Tuple, TypeVar, Optional
 from typing_extensions import TypedDict  # because we support 3.6.5
 
-Feel = NamedTuple(
-    "Feel",
-    [("energy", float), ("lyrics", float), ("dance", float), ("melody", float)],
-)
-
-
 class Song(TypedDict):
     title: str
 
