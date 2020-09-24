60e216bdf0553201b223040e6b581177492e8961
Alex <7alex7li@gmail.com>
2020-09-24 18:04:08 -0400

    Move tests

--- watsong/test/test_spotify.py
+++ watsong/test/test_spotify.py
@@ -3,79 +3,10 @@ import sys
 
 sys.path.append("..")  # Adds higher directory to python modules path.
 
-from watsong.spotify import get_songs, add_audio_features, create_playlist
-from watsong.structures import AlbumDescription
 
-
-class TestSpotify(unittest.TestCase):
-    def test_get_songs_single_artist_many_songs_without_features(self) -> None:
-        album_list = [
-            AlbumDescription("A girl between two worlds", ["Fatima Yamaha"]),
-        ]
-        songs, errors = get_songs(album_list)
-
-        self.assertEqual(
-            songs[0],
-            {
-                "title": "Between Worlds",
-                "uri": "spotify:track:0Vpdt3FsW8m7nC4FDk3rfw",
-                "features": {},
-            },
-        )
-        self.assertEqual(len(songs), 7)
-        self.assertIsNone(errors)
-
-    def test_get_songs_single_artist_many_songs_with_features(self) -> None:
-        album_list = [
-            AlbumDescription("A girl between two worlds", ["Fatima Yamaha"]),
-        ]
-        songs, errors = get_songs(album_list)
-        add_audio_features(songs)
-
-        self.assertEqual(
-            songs[0],
-            {
-                "title": "Between Worlds",
-                "uri": "spotify:track:0Vpdt3FsW8m7nC4FDk3rfw",
-                "features": {
-                    "energy": 0.611,
-                    "dance": 0.75,
-                    "lyrics": 0.0538,
-                    "valence": 0.546,
-                },
-            },
-        )
-        self.assertEqual(len(songs), 7)
-        self.assertIsNone(errors)
-
-    def test_get_songs_multi_artist_one_song_without_features(self) -> None:
-        album_list = [
-            AlbumDescription("Harder", ["Jax Jones", "Bebe Rexha"]),
-        ]
-        songs, errors = get_songs(album_list)
-
-        self.assertEqual(
-            songs[0],
-            {
-                "title": "Harder (with Bebe Rexha)",
-                "uri": "spotify:track:5ieQrVW2U70NFMg28mzlqC",
-                "features": {},
-            },
-        )
-        self.assertEqual(len(songs), 1)
-
-    def test_create_playlist(self) -> None:
-        album_list = [
-            AlbumDescription("A girl between two worlds", ["Fatima Yamaha"]),
-        ]
-        songs, errors = get_songs(album_list)
-        link = create_playlist(songs)
-        link2 = create_playlist(songs)
-        # TODO: Change lyrics and melody values when making those additionsblack
-        self.assertEqual(link, link2)
-        self.assertRegex(link, "https://open.spotify.com/embed/playlist/*")
-
-    # TODO: Once Sam finalizes the filter songs API, unit tests need to be made for it
+class Test(unittest.TestCase):
+    def test_add(self) -> None:
+        self.assertEqual(3 + 4, 7)
 
 
 if __name__ == "__main__":
--- watsong/watson.py
+++ watsong/watson.py
@@ -3,8 +3,20 @@ This file is a starter for any Watson stuff that needs to happen.
 """
 
 from typing import List
-
 from .structures import AlbumDescription, Result
+import os
+from ibm_watson import DiscoveryV1  # type: ignore
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator  # type: ignore
+
+
+apikey = os.getenv("DISCOVERY_API_KEY")
+environment_id = "26e276ef-e35e-4076-a190-bab90b5a4521"
+collection_id = "3789d265-dda2-465a-98fb-e804f1435317"
+service_url = "https://api.us-south.discovery.watson.cloud.ibm.com/instances/230365e2-48ca-4b2f-8a9e-3dba5fbe20ed"
+version = "2019-04-30"
+authenticator = IAMAuthenticator(apikey)
+discovery = DiscoveryV1(version=version, authenticator=authenticator)
+discovery.set_service_url(service_url)
 
 
 def get_albums(query: str) -> Result[List[AlbumDescription]]:
